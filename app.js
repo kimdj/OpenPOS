a1e9ea1c804540405bb1c91680c944fd01ae0673
David Kim <kim.david.j@gmail.com>
2017-07-14 18:28:28 -0700

    finished migrating OpenPOS-Auth into OpenPOS
    ready to link up the main app to the 'dashboard'

--- app.js
+++ app.js
@@ -1,167 +1,87 @@
-var app = angular.module('OpenPOS', []);
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
-app.controller('PosController', function ($scope) {
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
 
-	$scope.drinks = [{
-			id: 0,
-			name: "Still Water",
-			price: "1",
-    },
-		{
-			id: 1,
-			name: "Sparkling Water",
-			price: "1.10",
-    },
-		{
-			id: 2,
-			name: "Espresso",
-			price: "1.20",
-    },
-		{
-			id: 3,
-			name: "Cappuccino",
-			price: "1.30",
-    },
-		{
-			id: 4,
-			name: "Tea",
-			price: "1.90",
-    },
-		{
-			id: 5,
-			name: "Hot Chocolate",
-			price: "2.10",
-    },
-		{
-			id: 6,
-			name: "Coke",
-			price: "2.00",
-    },
-		{
-			id: 7,
-			name: "Orange Juice",
-			price: "1.90",
-    }];
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-	$scope.foods = [{
-			id: 8,
-			name: "Waffle",
-			price: "1.50",
-    },
-		{
-			id: 9,
-			name: "Brioche",
-			price: "1.30",
-    },
-		{
-			id: 10,
-			name: "Cheesecake",
-			price: "1.70",
-    },
-		{
-			id: 11,
-			name: "Sandwich",
-			price: "2.70",
-    },
-		{
-			id: 12,
-			name: "Donuts",
-			price: "1.90",
-    },
-		{
-			id: 13,
-			name: "Tortilla",
-			price: "1.90",
-    }];
+// Init App
+var app = express();
 
-	$scope.order = [];
-	$scope.new = {};
-	$scope.totOrders = 0;
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
-	var url = window.location.protocol + "://" + window.location.host + "/" + window.location.pathname;
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-	$scope.getDate = function () {
-		var today = new Date();
-		var mm = today.getMonth() + 1;
-		var dd = today.getDate();
-		var yyyy = today.getFullYear();
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-		var date = dd + "/" + mm + "/" + yyyy
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
 
-		return date
-	};
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
 
-	$scope.addToOrder = function (item, qty) {
-		var flag = 0;
-		if ($scope.order.length > 0) {
-			for (var i = 0; i < $scope.order.length; i++) {
-				if (item.id === $scope.order[i].id) {
-					item.qty += qty;
-					flag = 1;
-					break;
-				}
-			}
-			if (flag === 0) {
-				item.qty = 1;
-			}
-			if (item.qty < 2) {
-				$scope.order.push(item);
-			}
-		} else {
-			item.qty = qty;
-			$scope.order.push(item);
-		}
-	};
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-	$scope.removeOneEntity = function (item) {
-		for (var i = 0; i < $scope.order.length; i++) {
-			if (item.id === $scope.order[i].id) {
-				item.qty -= 1;
-				if (item.qty === 0) {
-					$scope.order.splice(i, 1);
-				}
-			}
-		}
-	};
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
-	$scope.removeItem = function (item) {
-		for (var i = 0; i < $scope.order.length; i++) {
-			if (item.id === $scope.order[i].id) {
-				$scope.order.splice(i, 1);
-			}
-		}
-	};
+// Connect Flash
+app.use(flash());
 
-	$scope.getTotal = function () {
-		var tot = 0;
-		for (var i = 0; i < $scope.order.length; i++) {
-			tot += ($scope.order[i].price * $scope.order[i].qty)
-		}
-		return tot;
-	};
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
-	$scope.clearOrder = function () {
-		$scope.order = [];
-	};
 
-	$scope.checkout = function (index) {
-		alert($scope.getDate() + " - Order Number: " + ($scope.totOrders + 1) + "\n\nOrder amount: $" + $scope.getTotal().toFixed(2) + "\n\nPayment received. Thanks.");
-		$scope.order = [];
-		$scope.totOrders += 1;
-	};
 
-	$scope.addNewItem = function (item) {
-		if (item.category === "Drinks") {
-			item.id = $scope.drinks.length + $scope.foods.length
-			$scope.drinks.push(item)
-			$scope.new = []
-			$('#myTab a[href="#drink"]').tab('show')
-		} else if (item.category === "Foods") {
-			item.id = $scope.drinks.length + $scope.foods.length
-			$scope.foods.push(item)
-			$scope.new = []
-			$('#myTab a[href="#food"]').tab('show')
-		}
-	};
+app.use('/', routes);
+app.use('/users', users);
 
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
