7e5a8fc2b915d049730d0812c6de44cd33f083fd
David Kim <kim.david.j@gmail.com>
2017-08-09 18:59:23 -0700

    many changes, refactored code
    added functionality to add products to the backend database
    fixed the drinks/foods/new item tabs issue
    	problem is related to bootstrap-jQuery conflict
    	changed jQuery and Bootstrap cdn's to older versions in pos.html

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express'),
 	app = express(),
 	mongojs = require('mongojs'),
 	db = mongojs('contactlist', ['contactlist']),
+	db2 = mongojs('productlist', ['productlist']),
 	bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+
+//  contact list  --------------------------------------------------------------------------------
 
 app.get('/contactlist', function (req, res) {
 	console.log("I received a GET request")
@@ -29,11 +32,50 @@ app.post('/contactlist', function (req, res) {
 app.delete('/contactlist/:id', function (req, res) {
 	var id = req.params.id;
 	console.log(id);
-	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+	db.contactlist.remove({
+		_id: mongojs.ObjectId(id)
+	}, function (err, doc) {
 		res.json(doc);
 	});
 });
 
+//  contact list  --------------------------------------------------------------------------------
+
+
+//  product list  --------------------------------------------------------------------------------
+
+app.get('/productlist', function (req, res) {
+	console.log("I received a GET request")
+
+	db2.productlist.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/productlist', function (req, res) {
+	console.log(req.body);
+
+	// insert the input data into the db
+	// and send the new data from the db back to the controller
+	db2.productlist.insert(req.body, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+app.delete('/productlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db2.productlist.remove({
+		_id: mongojs.ObjectId(id)
+	}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+//  product list  --------------------------------------------------------------------------------
+
+// start the server
 app.listen(3000);
 console.log("Server running on port 3000");
 
